380f175f674c584ebce88e59e42c0b07b66f9cc1
Mathias Teier <teier@kagent.at>
2017-04-02 22:21:29 +0200

    Added menus and changed Chord-Area to monospace font

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const {app, Menu, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -9,20 +9,39 @@ let win = null;
 app.on('ready', function () {
   win = new BrowserWindow({width: 1280, height: 720});
 
-  if (process.env.PACKAGE === 'true'){
+  //if (process.env.PACKAGE === 'true'){
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'dist/index.html'),
       protocol: 'file:',
       slashes: true
     }));
-  } else {
+/*  }else {
     win.loadURL(process.env.HOST);
     win.webContents.openDevTools();
-  }
+  }*/
   win.on('closed', function () {
     win = null;
   });
+  var menu = [{
+        label: "MuseChords-Desktop",
+        submenu: [
+            { label: "About MuesChords", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
 
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 });
 
 app.on('activate', function() {
